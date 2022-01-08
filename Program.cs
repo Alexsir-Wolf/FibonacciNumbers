b095eade8960c5ed8a3a2ba6d6d7b13f82dcce44
Alex Freitas <freitas.alex.soares@outlook.com>
2022-01-08 17:33:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 1 };
+            Console.WriteLine("Digite um numero para a quantidade da Sequência de Fibonacci: ");
+            int amount = int.Parse(Console.ReadLine());
+
+            while (list.Count < amount)
+            {
+                var previous = list[list.Count - 1];
+                var previous2 = list[list.Count - 2];
+
+                list.Add(previous + previous2);
+            }
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
